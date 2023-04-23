db295efc2cc5f2e6e9285fd1c17283c5e7f87c78
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-04-23 10:15:12 -0300

    new cli functions

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,36 @@
 import chalk from "chalk"
 import extractLinks from "./index.js"
+import fs from 'node:fs'
+
 const path = process.argv[2]
 
-async function processText(path) {
-    const links = await extractLinks(path)
-    console.log(chalk.yellow('list de links'), links)
+function printList(links, filename = '') {
+    console.log(chalk.bgYellow(`lista de links do arquivo ${path}/${filename}`), links)
 
 }
 
-processText(path)
+async function processText() {
+
+    try {
+        if (fs.lstatSync(path).isFile()) {
+            const links = await extractLinks(path)
+            printList(links)
+        } else if (fs.lstatSync(path).isDirectory()) {
+            const files = await fs.promises.readdir(path)
+            files.forEach(async (file) => {
+                const links = await extractLinks(`${path}/${file}`)
+                printList(links, file);
+            })
+        }
+
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.log(chalk.red('arquivo ou diretório não existe'))
+            return
+        }
+    }
+}
+
+
+
+processText()
